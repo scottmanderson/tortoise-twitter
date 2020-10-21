64c7eec693cca424e97fbc63178c3f8f9b2f6a29
Scott Anderson <30956117+scottmanderson@users.noreply.github.com>
2020-10-20 22:29:39 -0500

    first post method test including mongoose

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,11 +1,50 @@
 const app = require("../server");
+const connectDB = require("../config/db");
 const users = require("./users");
+const UserModel = require("../models/User");
 
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const request = require("supertest");
 const express = require("express");
 
+beforeAll(async () => {
+  const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI + "/test", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      });
+
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  };
+});
+
+afterAll(async () => {
+  UserModel.deleteOne({ name: "test_jest" });
+});
+
 describe("register route works", () => {
   test("register page loads", (done) => {
-    return request(app).get("/users/register").expect(200, done);
+    request(app).get("/users/register").expect(200, done);
+  });
+  test("register form post method", (done) => {
+    const newUser = request(app).post("/users/register").send({
+      name: "test_jest",
+      email: "test_jest@test.com",
+      password: "abcdefgh",
+      password2: "abcdefgh",
+      trackedHandlesStr: "aldaily, dan_abramov, juliagalef",
+      preferredTimeGMT: "22:00",
+    });
+    query = UserModel.findOne({ name: "test_jest" });
+    expect(query).toBeTruthy();
+
+    done();
   });
 });
